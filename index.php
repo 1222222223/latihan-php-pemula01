283b5004f3d86f8f21d9af4dc3608e32da43a597
Iven Lagowan <94290469+ivendf@users.noreply.github.com>
2022-09-14 13:32:26 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+// Pertemuan Satu
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+var_dump("Iven Lagowan");
+?>
+<html>
+    <head>
+        <title>Belajar PHP</title>
+    </head>
+    <body>
+        <h1>Selamat Datang<?php echo "Iven Lagowan"?></h1>
+    </body>
+</html>
---
+++ sacode-errom.php
@@ -0,0 +1,24 @@
+<?php
+// Pengulangan
+// For
+// While
+// Do.. While
+// Foreach : pengulangan khusus array
+
+
+// for( $i = 0; $i < 5; $i++ ) {
+//    echo "Iven <br>";}
+
+// $i = 0;{}
+// while( $i < 5 ){
+//   echo "Iven Lagowan! <br>";
+//  $i++;}
+
+// $i = 0;
+// do{
+//    echo "Iven Lagowan <br>";
+// $i++;
+// }while( $i < 5 );
+
+
+?>
